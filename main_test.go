4499dd1522b1d7a6a5bf089e94cfa4826a2530b4
Ryan Daniels <ryan.daniels@auth0.com>
2019-07-13 00:29:57 -0400

    chore: download and extract node binary

--- main_test.go
+++ main_test.go
@@ -19,3 +19,33 @@ func TestGetNodeBaseFolder(t *testing.T) {
 	assert.Nil(t, err)
 	assert.True(t, stat.IsDir())
 }
+
+func TestGetNodeURL(t *testing.T) {
+	version := "10.16.0"
+	app := NewApp()
+	t.Run("darwin-amd64", func(t *testing.T) {
+		url := app.getNodeURL(version, "darwin", "amd64")
+		assert.Equal(t, "https://nodejs.org/dist/v10.16.0/node-v10.16.0-darwin-x64.tar.gz", url)
+	})
+	t.Run("windows-amd64", func(t *testing.T) {
+		url := app.getNodeURL(version, "windows", "amd64")
+		assert.Equal(t, "https://nodejs.org/dist/v10.16.0/node-v10.16.0-win-x64.zip", url)
+	})
+	t.Run("linux-amd64", func(t *testing.T) {
+		url := app.getNodeURL(version, "linux", "amd64")
+		assert.Equal(t, "https://nodejs.org/dist/v10.16.0/node-v10.16.0-linux-x64.tar.xz", url)
+	})
+}
+
+func TestUnpackNodeBinary(t *testing.T) {
+	app := NewApp()
+	t.Run("darwin-amd64", func(t *testing.T) {
+		path := app.unpackNodeBinary("/tmp/node-v10.16.0-darwin-x64.tar.gz")
+	})
+	t.Run("windows-amd64", func(t *testing.T) {
+		path := app.unpackNodeBinary("/tmp/node-v10.16.0-win-x64.zip")
+	})
+	t.Run("linux-amd64", func(t *testing.T) {
+		path := app.unpackNodeBinary("/tmp/node-v10.16.0-linux-x64.tar.xz")
+	})
+}
